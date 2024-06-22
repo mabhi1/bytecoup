9faf0de1217d2c299bca1a52d8afdd01a80d1190
Abhishek Mishra <mabhishek0221@gmail.com>
2024-06-22 15:12:23 -0400

    warning fix

--- components/pages/home/testimonial-cards.tsx
+++ components/pages/home/testimonial-cards.tsx
@@ -25,7 +25,7 @@ export default function TestimonialCards({
 
   useEffect(() => {
     addAnimation();
-  }, [addAnimation]);
+  }, []);
   const [start, setStart] = useState(false);
   function addAnimation() {
     if (containerRef.current && scrollerRef.current) {
